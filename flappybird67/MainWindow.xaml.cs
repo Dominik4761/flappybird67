2dc9127f9dd30eeff5acca4e3b4d9d086fe74639
Petrik Diak <diak@petrik.hu>
2026-01-07 10:29:18 +0100

    szereplok, madar mozgas

--- flappybird67/MainWindow.xaml.cs
+++ flappybird67/MainWindow.xaml.cs
@@ -8,17 +8,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace flappybird67
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		DispatcherTimer timer = new DispatcherTimer();
+		double sebesseg = 0;
+		double gravitacio = 0.6;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			timer.Interval = TimeSpan.FromMilliseconds(20);
+			timer.Tick += JatekLoop;
+			timer.Start();
+
+			KeyDown += Ablak_KeyDown;
+		}
+
+		void JatekLoop(object sender, EventArgs e)
+		{
+			sebesseg += gravitacio;
+			Canvas.SetTop(pengo, Canvas.GetTop(pengo) + sebesseg);
+		}
+
+		void Ablak_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Space)
+			{
+				sebesseg = -8;
+			}
+		}
+	}
 }
